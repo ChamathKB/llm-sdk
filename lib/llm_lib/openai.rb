c446453c208953182f0ac8cc6b440d28e5d23a52
ChamathKB <amsmckbattanayaka@std.appsc.sab.ac.lk>
2023-07-11 01:49:24 +0530

    add API endpoint for openai

--- lib/llm_lib/openai.rb
+++ lib/llm_lib/openai.rb
@@ -1,13 +1,21 @@
 class LlmLib::OpenAI
 
-    def self.send(apikey, body, mode, model, n, p, q, temperature, top_p, top_k, stream, logprobs, stop, presence_penalty, frequency_penalty, max_l)
+    def self.send(apikey, body, model, prompt, max_tokens, temperature=0, top_p=1, n=1, stream=false, logprobs=nil, stop="\n")
 
         return LlmLib::Restclient.post(
             body: {
-
+		        "model" => model,
+                "prompt" => prompt,
+                "max_tokens" => max_tokens,
+                "temperature" => temperature,
+                "top_p" => top_p,
+                "n" => n,
+                "stream" => stream,
+                "logprobs" => logprobs,
+                "stop" => stop,
 
             },
-            url: "https://api.openai.com/v1/engines/#{model}/completions",
+            url: "https://api.openai.com/v1/completions",
             apikey: apikey,
         )
     end
