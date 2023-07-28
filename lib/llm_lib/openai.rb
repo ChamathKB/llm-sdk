82ecbb2983bb67ff17cca43c0147b51c9084920b
ChamathKB <amsmckbattanayaka@std.appsc.sab.ac.lk>
2023-07-29 01:21:11 +0530

    update openai client

--- lib/llm_lib/openai.rb
+++ lib/llm_lib/openai.rb
@@ -1,22 +1,26 @@
+require 'llm_lib'
+
 class LlmLib::OpenAI
 
-    def self.send(apikey, body, model, prompt, max_tokens, temperature=0, top_p=1, n=1, stream=false, logprobs=nil, stop="\n")
+    def self.send(apikey, model, prompt, max_tokens, temperature=0, top_p=1, n=1, stream=false, stop="\n")
 
         return LlmLib::Restclient.post(
             body: {
 		        "model" => model,
-                "prompt" => prompt,
+                # "prompt" => prompt,
+                "messages" => [{ role: "user", content: prompt}],
                 "max_tokens" => max_tokens,
                 "temperature" => temperature,
                 "top_p" => top_p,
                 "n" => n,
                 "stream" => stream,
-                "logprobs" => logprobs,
+                # "logprobs" => logprobs,
                 "stop" => stop,
 
             },
-            url: "https://api.openai.com/v1/completions",
+            url: "https://api.openai.com/v1/chat/completions",
             apikey: apikey,
+            # headers: { 'Authorization' => "Bearer #{apikey}" },
         )
     end
 end
