f695c317533256f5ff7e211f08ea7f9c705f82ec
ChamathKB <amsmckbattanayaka@std.appsc.sab.ac.lk>
2023-07-29 01:20:46 +0530

    update huggingface client

--- lib/llm_lib/huggingface.rb
+++ lib/llm_lib/huggingface.rb
@@ -1,15 +1,16 @@
 class LlmLib::HuggingFace
 
-    def self.send(apikey, body, model, query)
+    def self.send(apikey, model, query)
 
         return LlmLib::Restclient.post(
             body: {
-                "model" => model,
-                "query" => query,
+                # "model" => model,
+                "inputs" => query,
 
             },
             url: "https://api-inference.huggingface.co/models/#{model}",
             apikey: apikey
+            # headers:{ 'Authorization' => "Bearer #{apikey}" }
         )
     end
 end
